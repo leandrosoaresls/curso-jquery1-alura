fe390d95c653c022c0b42da886a1655ab097f628
leandrosoaresls <leandro_soares.ls@hotmail.com>
2018-08-05 01:01:43 -0300

    atualizando projeto

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,19 @@
-console.log("Olá mundo!");
+const frase = $(".frase").text();
+const numPalavras = frase.split(" ").length;
+const tamanhoFrase = $("#tamanho-frase");
+tamanhoFrase.text(numPalavras);
+
+const campo = $(".campo-digitacao");
+campo.on("input", function(){
+    const conteudoDigitado = campo.val();
+
+    const qtdPalavras = conteudoDigitado.split(/\S+/).length - 1;
+    $(".contador-palavras").text(qtdPalavras);
+
+    const conteudoSemEspaco = conteudoDigitado.replace(/\s+/g,'');
+
+    console.log(conteudoSemEspaco);
+
+    const qtdCaracteres = conteudoSemEspaco.length;
+    $(".contador-caracteres").text(qtdCaracteres);
+});
